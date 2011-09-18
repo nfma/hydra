4b55f3e3f30560c4aacf96f55778604075db4ef8
nfma <nuno.filipe.marques@gmail.com>
2011-09-19 00:14:08 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject hydra "1.0.0-SNAPSHOT"
+  :description "App that publishes any status updates and shares on a social network in other social networks"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ test/hydra/test/core.clj
@@ -0,0 +1,6 @@
+(ns hydra.test.core
+  (:use [hydra.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
